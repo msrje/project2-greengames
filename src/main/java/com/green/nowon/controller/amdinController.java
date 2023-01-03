943cf838f24c5fd4c4e6729d505cd06dc006ed7b
Administrator <Administrator@304-10>
2023-01-03 15:08:56 +0900

    admin페이지 기본틀 디자인

---
+++ src/main/java/com/green/nowon/controller/amdinController.java
@@ -0,0 +1,15 @@
+package com.green.nowon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class amdinController {
+	
+	@GetMapping("/admin")
+	public String admin() {
+		return "admin/ggAdmin";
+	}
+	
+	
+}
