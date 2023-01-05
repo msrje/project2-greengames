62c1c4513451b262920fc5dcf2578af6d7c91371
Administrator <Administrator@304-10>
2023-01-05 15:29:28 +0900

    230105 확인

--- src/main/java/com/green/nowon/domain/entity/member/MemberEntityRepository2.java
+++ src/main/java/com/green/nowon/domain/entity/member/MemberEntityRepository2.java
@@ -1,11 +1,13 @@
 package com.green.nowon.domain.entity.member;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberEntityRepository extends JpaRepository<MemberEntity, Long>{
+public interface MemberEntityRepository2 extends JpaRepository<MemberEntity, Long>{
 
-	MemberEntity findById(String id);
+	Optional<MemberEntity> findById(String id);
 	
 }
