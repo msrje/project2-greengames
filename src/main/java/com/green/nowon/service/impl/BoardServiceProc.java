db6f2e2bddbfe64b849f97dd36e4463188745d12
Administrator <Administrator@304-19>
2023-01-09 11:55:33 +0900

    0109_board

--- src/main/java/com/green/nowon/service/impl/BoardServiceProc.java
+++ src/main/java/com/green/nowon/service/impl/BoardServiceProc.java
@@ -1,8 +1,18 @@
 package com.green.nowon.service.impl;
 
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
+import com.green.nowon.domain.dto.board.BoardDetailDTO;
+import com.green.nowon.domain.dto.board.BoardListDTO;
 import com.green.nowon.domain.dto.board.BoardSaveDTO;
 import com.green.nowon.domain.dto.board.BoardUpdateDTO;
 import com.green.nowon.domain.entity.board.BoardEntityRepository;
@@ -20,17 +30,30 @@ public class BoardServiceProc implements BoardService{
 	
 	@Override
 	public void getListAll(int page, Model model) {
-		// TODO Auto-generated method stub
+		//board list를 페이지로 전송
+		int size=10;
+		Sort sort=Sort.by(Direction.DESC, "bno");
+		Pageable pageable=PageRequest.of(page-1, size, sort);
+		Page<BoardEntity> result=repository.findAll(pageable);
 		
+		model.addAttribute("p", result);
+		model.addAttribute("list", result.stream()
+				.map(BoardListDTO::new)
+				.collect(Collectors.toList()));
 	}
 
 	@Override
 	public void sendDetail(long bno, Model model) {
-		// TODO Auto-generated method stub
+		model.addAttribute("detail", repository.findById(bno)
+				.map(BoardDetailDTO::new)
+				.orElseThrow());
 		
 	}
 
-
+	@Override
+	public void save(BoardSaveDTO dto, String name) {
+	}
+	
 	@Override
 	public void save(BoardSaveDTO dto) {
 		
@@ -44,19 +67,26 @@ public class BoardServiceProc implements BoardService{
 
 	@Override
 	public void delete(long bno) {
-		// TODO Auto-generated method stub
-		
+		repository.deleteById(bno);
 	}
 
 	@Override
 	public void update(long bno, BoardUpdateDTO dto) {
-		// TODO Auto-generated method stub
+		Optional<BoardEntity> result= repository.findById(bno);
+		
+		//존재하면 수정
+		if(result.isPresent()) {
+			BoardEntity entity=result.get();
+			entity.update(dto);
+			//업데이트 반영
+			repository.save(entity);//이미 존재하는 Pk이면 수정됨
+		}
 		
 	}
 
 	@Override
 	public void updateProc(long bno, BoardUpdateDTO dto) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
