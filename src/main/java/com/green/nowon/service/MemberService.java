b2dc4d114006ac63069780729468f8d93708b41c
Administrator <Administrator@304-10>
2023-01-09 15:34:09 +0900

    0109급여리스트

--- src/main/java/com/green/nowon/service/MemberService.java
+++ src/main/java/com/green/nowon/service/MemberService.java
@@ -7,6 +7,9 @@ import com.green.nowon.domain.dto.memberDTO.MemberInsertDTO;
 
 public interface MemberService {
 	void save(MemberInsertDTO mdto,AddressInsertDTO adto);
+	
 	void detail(long mno, Model model ,Model model2) ;
+	
+	void list(Model model);
 
 }
