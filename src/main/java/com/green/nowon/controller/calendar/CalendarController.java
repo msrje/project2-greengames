b565a795dc39b2b397d61a4149ffe5ca737d2f1a
user <user@DESKTOP-PQ4U44G>
2023-01-09 11:51:44 +0900

    0109

--- src/main/java/com/green/nowon/controller/calendar/CalendarController.java
+++ src/main/java/com/green/nowon/controller/calendar/CalendarController.java
@@ -1,32 +1,20 @@
 package com.green.nowon.controller.calendar;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class CalendarController {
-	
-	//캘린더 페이지로 가기
+
+	// 캘린더 페이지로 가기
 	@GetMapping("/schedule/calendar")
 	public String calendar() {
 		return "/admin/calendar";
 	}
 	
-	//일정 작성 
-	@ResponseBody
-	@PostMapping("/schedule/calendar")
-	public Map<String, Object> calendarSave(@RequestParam Map<String, Object> param ) {
-		Map<String, Object> cal = new HashMap<String, Object>();
-		cal.put("title", "제목임다");
-		System.out.println(param.get("title"));
-		return cal; 
-	}
-}
 
 	
+}
