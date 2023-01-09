d47046a5c1ba92a4267737c93b15c1785b816518
Administrator <Administrator@304-10>
2023-01-09 11:38:15 +0900

    0109 마이페이지 개인정보까지 끝

--- src/main/java/com/green/nowon/service/mypage/MyPageServiceProcess.java
+++ src/main/java/com/green/nowon/service/mypage/MyPageServiceProcess.java
@@ -5,23 +5,31 @@ import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
 import com.green.nowon.domain.dto.memberDTO.AddressDetailDTO;
+import com.green.nowon.domain.dto.memberDTO.MemberDetailDTO;
 import com.green.nowon.domain.entity.member.AddressEntityRepsoitory;
-import com.green.nowon.domain.entity.member.MemberEntityRepository2;
+import com.green.nowon.domain.entity.member.MemberEntityRepository;
 
 @Service
 public class MyPageServiceProcess implements MyPageService{
 
+	
 	@Autowired
-	private MemberEntityRepository2 mRepo;
+	private MemberEntityRepository mRepo;
 	@Autowired
 	private AddressEntityRepsoitory aRepo;
 	
 	
+	
+
+
 	@Override
-	public void myPageBaseAddr(String id, Model model) {
-		model.addAttribute("mpageaddr",aRepo.findByBaseAndMemberId(true,id)
-				.map(AddressDetailDTO::new).orElseThrow()
+	public void info(long mno, Model model,Model model2) {
+		model.addAttribute("detail",mRepo.findById(mno)
+				.map(MemberDetailDTO::new).orElseThrow()
 				);
+		model2.addAttribute("mpageaddr",aRepo.findByMember_mno(mno)
+				.map(AddressDetailDTO::new)
+				.orElseThrow());
 	}
 
 
