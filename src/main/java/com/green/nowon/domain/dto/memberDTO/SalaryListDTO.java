a870ee06ff1b9a2b9a59ac3bc7dc540dce275a13
YoungJuLee <46048175+wykaw@users.noreply.github.com>
2023-01-11 12:11:27 +0900

    2023-01-11 12:10 조직도 리스트 띄우기 css미적용 및 salaryListDTO

---
+++ src/main/java/com/green/nowon/domain/dto/memberDTO/SalaryListDTO.java
@@ -0,0 +1,28 @@
+package com.green.nowon.domain.dto.memberDTO;
+
+import com.green.nowon.domain.entity.member.MemberEntity;
+
+import lombok.Data;
+
+@Data
+public class SalaryListDTO {
+	
+	private long mno;
+	private String id;
+	private String name;
+	private String pass;
+	private String phone;
+	
+	private String profileUrl;
+	
+	public SalaryListDTO(MemberEntity e){
+		this.mno=e.getMno();
+		this.id=e.getId();
+		this.name = e.getName();
+		this.pass = e.getPass();
+		this.phone=e.getPhone();
+		this.profileUrl=e.defImg().getUrl()+e.defImg().getNewName();
+		
+	}
+	
+}
--- src/main/java/com/green/nowon/domain/entity/member/MemberEntity.java
+++ src/main/java/com/green/nowon/domain/entity/member/MemberEntity.java
@@ -1,6 +1,8 @@
 package com.green.nowon.domain.entity.member;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CollectionTable;
@@ -13,6 +15,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -61,6 +64,10 @@ public class MemberEntity extends BaseDateEntity{
 	private String phone;//번호
 	
 	@Builder.Default
+	@OneToMany(mappedBy = "member")
+	List<ProfileEntity> profile=new ArrayList<>();
+	
+	@Builder.Default
 	@CollectionTable(name = "GgDeploy")
 	@Enumerated(EnumType.STRING)//직책
 	@ElementCollection(fetch = FetchType.EAGER)
@@ -75,6 +82,14 @@ public class MemberEntity extends BaseDateEntity{
 		this.phone = dto.getPhone();
 		return this;
 	}
-	
-	
+	/**
+	 * 대표이미지 없는데 없으면 @builder가 안먹힘
+	 * @return
+	 */
+	public ProfileEntity defImg() {
+		for(ProfileEntity pimg:profile) {
+			return pimg;
+		}
+		return profile.get(0);
+	}
 }
