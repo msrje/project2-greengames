b57b5e2f9849341a3f5d0411d37330802ce48c22
msrje <111475691+msrje@users.noreply.github.com>
2023-01-12 15:26:43 +0900

    Update CalendarEntityRepository.java

--- src/main/java/com/green/nowon/domain/entity/schedule/CalendarEntityRepository.java
+++ src/main/java/com/green/nowon/domain/entity/schedule/CalendarEntityRepository.java
@@ -1,16 +1,19 @@
 package com.green.nowon.domain.entity.schedule;
 
-import java.util.Map;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.green.nowon.domain.dto.schedule.calendarDTO;
+import com.green.nowon.domain.dto.schedule.CalendarDTO;
 
 @Repository
 public interface CalendarEntityRepository extends JpaRepository<CalendarEntity, Long>{
 
 	// 캘린더 insert
-	void save(calendarDTO dto);
+	void save(CalendarDTO dto);
+	
+	// 캘린더 select
+	List<CalendarEntity> findAllByMember_mno(long userMno);
 
 }
