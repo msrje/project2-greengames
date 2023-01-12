b606fb7b5257d0db5351d53472f1899dc2ae9cbb
msrje <111475691+msrje@users.noreply.github.com>
2023-01-12 15:25:44 +0900

    Update CalendarServiceProc.java

--- src/main/java/com/green/nowon/service/impl/CalendarServiceProc.java
+++ src/main/java/com/green/nowon/service/impl/CalendarServiceProc.java
@@ -1,11 +1,12 @@
 package com.green.nowon.service.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.green.nowon.domain.dto.schedule.calendarDTO;
+import com.green.nowon.domain.dto.schedule.CalendarDTO;
 import com.green.nowon.domain.entity.member.MemberEntity;
 import com.green.nowon.domain.entity.schedule.CalendarEntity;
 import com.green.nowon.domain.entity.schedule.CalendarEntityRepository;
@@ -20,40 +21,22 @@ public class CalendarServiceProc implements CalendarService {
 
 	// 캘린더 insert
 	@Override
-	public void save(calendarDTO dto) {
+	public void save(long userMno, CalendarDTO dto) {
 		CalendarEntity entity=CalendarEntity.builder()
 				.cTitle(dto.getCTitle()).cStartTime(dto.getCStartTime()).cEndTime(dto.getCEndTime())
-				.member(MemberEntity.builder().mno(dto.getMno()).build())
+				.member(MemberEntity.builder().mno(userMno).build())
 				.build();
 		crepo.save(entity);
 		
 	}
-
+	
+	// 캘린더 select
 	@Override
-	public calendarDTO getListAll() {
-		List<CalendarEntity> dto = crepo.findAll();
-		System.out.println(dto);
-		// TODO Auto-generated method stub
-		return null;
+	public List<CalendarDTO> getList(long userMno) {
+		return crepo.findAllByMember_mno(userMno).stream()
+				.map(CalendarDTO::new)
+				.collect(Collectors.toList());
 	}
-	
-//	@Override
-//	public void save(Map<String, Object> param) throws ParseException {
-//		
-//		SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
-//		Date ari = dt.parse((String) param.get("start"));
-//		Date cherry = dt.parse((String) param.get("end"));
-//		System.out.println("프로세스>>>"+param+"\n"+ari+"\n"+cherry);
-//		CalendarEntity entity=CalendarEntity.builder()
-//				.cTitle((String) param.get("title")).cStartTime(ari)
-//				.cEndTime(cherry).member( (MemberEntity) param.get("mno"))
-////				.cEndTime(cherry).member((MemberEntity)param.get("member"))
-//				.build();
-//		crepo.save(entity);
-//		
-//		
-//	}
-	
 	
 
 }
