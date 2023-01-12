5d468418b2dd013267768d275697eb88ed0f2868
msrje <111475691+msrje@users.noreply.github.com>
2023-01-12 15:25:27 +0900

    Update CalendarService.java

--- src/main/java/com/green/nowon/service/CalendarService.java
+++ src/main/java/com/green/nowon/service/CalendarService.java
@@ -1,14 +1,15 @@
 package com.green.nowon.service;
 
-import java.text.ParseException;
-import java.util.Map;
+import java.util.List;
 
-import com.green.nowon.domain.dto.schedule.calendarDTO;
+import com.green.nowon.domain.dto.schedule.CalendarDTO;
 
 public interface CalendarService {
-
-	calendarDTO getListAll();
 	// 캘린더 insert
-	void save(calendarDTO dto);
+	void save(long userMno , CalendarDTO dto);
+
+	// 캘린더 select
+	List<CalendarDTO> getList(long userMno);
+
 
 }
