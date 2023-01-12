980cf067ef9d021225fd7576f3ba0b38110085fa
msrje <111475691+msrje@users.noreply.github.com>
2023-01-12 15:23:56 +0900

    Update calendarDTO.java

--- src/main/java/com/green/nowon/domain/dto/schedule/calendarDTO.java
+++ src/main/java/com/green/nowon/domain/dto/schedule/calendarDTO.java
@@ -6,20 +6,34 @@ import com.green.nowon.domain.entity.member.MemberEntity;
 import com.green.nowon.domain.entity.schedule.CalendarEntity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Data
-public class calendarDTO {
+public class CalendarDTO {
 	
 	private String cTitle; 
 	private Date cStartTime;
 	private Date cEndTime;
+	private long cno;
 	private long mno;
 	
 	public CalendarEntity toCalendarEntity() {
 		
 		return CalendarEntity.builder()
-				.cTitle(cTitle).cStartTime(cStartTime).cEndTime(cEndTime).member(MemberEntity.builder().mno(mno).build())
+				.cTitle(cTitle).cStartTime(cStartTime).cEndTime(cEndTime)
+				.member(MemberEntity.builder().mno(mno).build())
 				.build();
 	}
+
+	public CalendarDTO(CalendarEntity e) {
+		this.cTitle = e.getCTitle();
+		this.cStartTime = e.getCStartTime();
+		this.cEndTime = e.getCEndTime();
+		this.cno = e.getCno();
+	}
+	
+	
+
 	
 }
