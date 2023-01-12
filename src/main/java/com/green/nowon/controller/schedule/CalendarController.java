74d826aa69f3cd393f77c676986dc266fa97c67c
msrje <111475691+msrje@users.noreply.github.com>
2023-01-12 10:21:57 +0900

    Update CalendarController.java

--- src/main/java/com/green/nowon/controller/schedule/CalendarController.java
+++ src/main/java/com/green/nowon/controller/schedule/CalendarController.java
@@ -1,7 +1,10 @@
 package com.green.nowon.controller.schedule;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,17 +28,20 @@ public class CalendarController {
 		return "/schedule/calendar";
 	}
 	// 캘린더 select
-	@GetMapping("/schedule/calendar/{mno}")
-	public String calendarSelect(@PathVariable long mno){
-		System.out.println(cService.getListAll());
+	@GetMapping("/schedule/calendar/{cno}")
+	public String calendarSelect(@PathVariable long cno, Model model){
+		System.out.println("CalendarController cno>>>>>"+cno);
+		List<calendarDTO> dto = cService.findById(cno);
+		System.out.println("CalendarController dto>>>>>"+dto.toString());
+		model.addAttribute("data", dto);
 		return "/schedule/calendar";
 	}
 	// 캘린더 insert
 	@PostMapping("/schedule/calendar")
 	public String calendarInsert(@RequestBody calendarDTO dto){
-		
+
 		//임시 로그인 방편
-		dto.setMno(4);
+		dto.setMno(5);
 		cService.save(dto);
 		return "/schedule/calendar";
 	}
