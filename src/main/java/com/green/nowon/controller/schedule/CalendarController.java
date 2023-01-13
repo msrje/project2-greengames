2ddb24a5b9255afc76262b6e445088b7f6937ed3
user <user@DESKTOP-PQ4U44G>
2023-01-13 17:04:33 +0900

    0113

--- src/main/java/com/green/nowon/controller/schedule/CalendarController.java
+++ src/main/java/com/green/nowon/controller/schedule/CalendarController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.green.nowon.domain.dto.schedule.CalendarDTO;
@@ -36,7 +35,6 @@ public class CalendarController {
 	@ResponseBody
 	@GetMapping("/schedule/calendar/{userMno}")
 	public List<CalendarDTO> calendarSelect(@PathVariable long userMno) {
-		System.err.println("CalendarController userMno>>>>>" + userMno);
 		return cService.getList(userMno);
 	}
 
@@ -44,8 +42,6 @@ public class CalendarController {
 	@ResponseBody
 	@PostMapping("/schedule/calendar/{userMno}")
 	public String calendarInsert(@PathVariable long userMno, @RequestBody CalendarDTO dto) {
-		System.err.println("CalendarController userMno>>>>" + userMno);
-		System.err.println("controller DTO>>>>>" + dto);
 		cService.save(userMno, dto);
 		return "/schedule/calendar";
 	}
@@ -54,10 +50,7 @@ public class CalendarController {
 	@ResponseBody
 	@DeleteMapping("/schedule/calendar/{userMno}")
 	public String delete(@PathVariable long userMno, @RequestBody List<Map<String, Object>> param) {
-		System.err.println("CalendarController 삭제>>>>" + "작동");
-		System.err.println("CalendarController param>>>>"+param.get(0).get("cno"));
 		long cno = Integer.parseInt((String.valueOf(param.get(0).get("cno"))));
-		System.err.println("CalendarController cno>>>"+cno);
 		cService.delete(cno);
 		return "redirect:/schedule/calendar";
 	}
